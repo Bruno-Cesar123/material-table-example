ab11f2a25adfcf327f3fe81f504fa60bb508159d
Bruno Cesar <brunocesar12386@gmail.com>
2021-11-30 21:43:14 -0300

    feat: create header component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
+import { Header } from './components/Header';
+
+import './styles/global.css'
+
 function App() {
   return (
-    <h1>Hello world</h1>
+    <Header />
   );
 }
 
---
+++ src/components/Header/index.js
@@ -0,0 +1,10 @@
+
+import styles from './styles.module.css';
+
+export function Header() {
+  return (
+    <header className={styles.header}>
+      <h1>Tabela de dados</h1>
+    </header>
+  )
+}
