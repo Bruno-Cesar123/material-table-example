4fd60687f9ffd57221cfa152386eedb38ac33985
Bruno Cesar <brunocesar12386@gmail.com>
2021-12-05 20:35:22 -0300

    feat: connet table with api

--- src/App.js
+++ src/App.js
@@ -7,34 +7,22 @@ import './styles/global.css'
 
 // https://jsonplaceholder.typicode.com/todos
 
-const empList = [
 
-  {id: 1, name: 'Bruno', email: 'brunocesar@gmail.com', status: 0, role: 1},
-  {id: 2, name: 'Bruno', email: 'brunocesar@gmail.com', status: 1, role: 0},
-  {id: 3, name: 'Bruno', email: 'brunocesar@gmail.com', status: 0, role: 3},
-  {id: 4, name: 'Bruno', email: 'brunocesar@gmail.com', status: 1, role: 2},
-]
-
-const empStatus=[
-  {id: 0, title: 'deactive'},
-  {id: 1, title: 'active'},
-]
 
 function App() {
-  const [data, setData] = useState(empList);
-  const [status, setStatus] = useState({})
+  const [data, setData] = useState([]);
   const columns = [
     { title: 'id', field: 'id' },
+    { title: 'username', field: 'username'},
     { title: 'name', field: 'name' },
     { title: 'email', field: 'email' },
-    { title: 'Status', field: 'status', lookup:status },
-    { title: 'role', field: 'role', lookup:{0:'Associte', 1: 'Senior Associate', 2: 'Architect', 3: 'Manager'} },
+    { title: 'phone', field: 'phone'},
   ]
 
   useEffect(() => {
-    const status = {}
-    empStatus.map(row => status[row.id]=row.title)
-    setStatus(status)
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(resp=>resp.json())
+    .then(resp=>setData(resp))
   }, [])
 
   return (
